aa9b5fe8015c6b312b4a9a168114eba36e0801e2
sorosz89 <os.sandor@gmail.com>
2018-03-19 15:33:35 +0100

    Update career_site.js

--- features/step_definitions/career_site.js
+++ features/step_definitions/career_site.js
@@ -2,30 +2,20 @@
 
 module.exports = function () {
 
-  let findButton = driver.findElement(by.css('.job-search__submit'));
-  let searchResult = driver.findElement(by.css('.search-result'));
-  let searchResultListItem = driver.findElement(by.css('.search-result__item-info'));
-  let searchResultListItemName = driver.findElement(by.css('.search-result__item-name'));
-  let searchResultListItemLocation = driver.findElement(by.css('.search-result__location'));
-  let searchResultListItemPriority = driver.findElement(by.css('.search-result__item-type'));
-  let searchResultListItemDescription = driver.findElement(by.css('.search-result__item-description'));
-  let roleInputField = driver.findElement(by.css('.job-search__input'));
-  let locationBox = driver.findElement(by.id('select-box-location-d2-container'));
-
   this.Given(/^the EPAM career site is loaded$/, () => {
     driver.get('https://www.epam.com/careers');
   });
 
   this.When(/^the Find button is clicked$/, () => {
-    return findButton.click();
+    return driver.findElement(by.css('.job-search__submit')).click();
   });
 
   this.When(/^the (.*) role is entered$/, role => {
-    return roleInputField.sendKeys(role);
+    return driver.findElement(by.css('.job-search__input')).sendKeys(role);
   });
 
   this. When(/^the country Hungary is selected$/, country => {
-    locationBox.click();
+    driver.findElement(by.id('select-box-location-d2-container')).click();
     let locationItem = driver.findElement(by.xpath('//li[contains(@aria-label,"' + country + '")]'));
     return locationItem.click();
   });
@@ -36,27 +26,27 @@ module.exports = function () {
   });
 
   this.Then(/^the available jobs are (displayed|hidden)$/, (state) => {
-    return expect(searchResult.isDisplayed()).to.eventually.be.equal(state === 'displayed');
+    return expect(driver.findElement(by.css('.search-result')).isDisplayed()).to.eventually.be.equal(state === 'displayed');
   });
 
   this.Then(/^an open position should be (displayed|hidden)$/, state => {
-    return expect(searchResultListItem.isDisplayed()).to.eventually.be.equal(state === 'displayed');
+    return expect(driver.findElement(by.css('.search-result__item-info')).isDisplayed()).to.eventually.be.equal(state === 'displayed');
   });
 
   this.Then(/^the title of the position should be (.*)$/, role => {
-    return expect(searchResultListItemName.getText()).to.eventually.equal(role);
+    return expect(driver.findElement(by.css('.search-result__item-name')).getText()).to.eventually.equal(role);
   });
 
   this.Then(/^the location of the position should be (.*)$/, location => {
-    return expect(searchResultListItemLocation.getText()).to.eventually.equal(location);
+    return expect(driver.findElement(by.css('.search-result__location')).getText()).to.eventually.equal(location);
   });
 
   this.Then(/^the priority of the position should be (.*)$/, priority => {
-    return expect(searchResultListItemPriority.getText()).to.eventually.equal(priority);
+    return expect(driver.findElement(by.css('.search-result__item-type')).getText()).to.eventually.equal(priority);
   });
 
   this.Then(/^the description of the position should start with: (.*)$/, desc => {
-    return searchResultListItemDescription.getText().then(text => expect(text.startsWith(desc)).to.be.equal(true));
+    return driver.findElement(by.css('.search-result__item-description')).getText().then(text => expect(text.startsWith(desc)).to.be.equal(true));
   });
 
   this.Then(/^And the following skills are selected: Software Engineering, Software Test Engineering$/, (data) => {
